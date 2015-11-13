8f565146b4ce812aa21d521b55ee60e1449a3664
Kyle Mayes <kyle@mayeses.com>
2015-11-13 16:37:35 -0500

    Add Clang struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,47 @@
 
 #[macro_use]
 extern crate bitflags;
+#[macro_use]
+extern crate lazy_static;
 
 extern crate libc;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+
 pub mod ffi;
+
+//================================================
+// Structs
+//================================================
+
+// Clang _________________________________________
+
+lazy_static! { static ref AVAILABLE: AtomicBool = AtomicBool::new(true); }
+
+/// An empty type which prevents the use of this library from multiple threads.
+pub struct Clang;
+
+impl Clang {
+    //- Constructors -----------------------------
+
+    /// Constructs a new `Clang`.
+    ///
+    /// Only one instance of `Clang` is allowed at a time.
+    ///
+    /// # Failures
+    ///
+    /// * an instance of `Clang` already exists
+    pub fn new() -> Result<Clang, ()> {
+        if AVAILABLE.swap(false, Ordering::Relaxed) {
+            Ok(Clang)
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl Drop for Clang {
+    fn drop(&mut self) {
+        AVAILABLE.store(true, Ordering::Relaxed);
+    }
+}
