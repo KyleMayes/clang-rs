48534c8d340195e9fc219de1a7e223e410b9d8a0
Kyle Mayes <kyle@mayeses.com>
2015-12-21 21:03:46 -0500

    Add example

---
+++ examples/structs.c
@@ -0,0 +1,20 @@
+#ifndef _STRUCTS_H_
+#define _STRUCTS_H_
+
+struct A {
+    int a, b, c, d;
+};
+
+struct B {
+    long a, b, c, d;
+};
+
+struct C {
+    float a, b, c, d;
+};
+
+struct D {
+    double a, b, c, d;
+};
+
+#endif
---
+++ examples/structs.rs
@@ -0,0 +1,34 @@
+extern crate clang;
+
+use clang::*;
+
+fn main() {
+    // Acquire an instance of `Clang`
+    let clang = Clang::new().unwrap();
+
+    // Create a new `Index`
+    let index = Index::new(&clang, false, false);
+
+    // Parse a source file into a translation unit
+    let tu = TranslationUnit::from_source(
+        &index, "examples/structs.c", &[], &[], ParseOptions::default()
+    ).unwrap();
+
+    // Get the structs in this translation unit
+    let structs = tu.get_entity().get_children().into_iter().filter(|e| {
+        e.get_kind() == EntityKind::StructDecl
+    }).collect::<Vec<_>>();
+
+    // Print information about the structs
+    for struct_ in structs {
+        let type_ =  struct_.get_type().unwrap();
+        let size = type_.get_sizeof().unwrap();
+        println!("struct: {:?} (size: {} bytes)", struct_.get_name().unwrap(), size);
+
+        for field in struct_.get_children() {
+            let name = field.get_name().unwrap();
+            let offset = type_.get_offsetof(&name).unwrap();
+            println!("    field: {:?} (offset: {} bits)", name, offset);
+        }
+    }
+}
