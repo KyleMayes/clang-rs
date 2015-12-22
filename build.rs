df43807247eb2bec311b22857f5c449bd8280651
Kyle Mayes <kyle@mayeses.com>
2015-12-22 14:40:35 -0500

    Enable raw_pointer_derive lint on <=1.5.* builds

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate rustc_version;
 
 fn main() {
-    if rustc_version::version_matches("1.5.*") {
-        println!("cargo:rustc-cfg={}", "rustc_1_5");
+    if rustc_version::version_matches("<=1.5.*") {
+        println!("cargo:rustc-cfg={}", "lte_rustc_1_5");
     }
 }
