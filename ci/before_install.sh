ede5505fb6bc5629422982f2ef6fdc7421e661cc
Kyle Mayes <kyle@mayeses.com>
2016-07-18 08:04:54 -0400

    Rework Travis CI scripts

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -6,15 +6,15 @@ function llvm_version_triple() {
     elif [ "$1" == "3.6" ]; then
         echo "3.6.2"
     elif [ "$1" == "3.7" ]; then
-        echo "3.7.1"
+        echo "3.7.0"
     elif [ "$1" == "3.8" ]; then
         echo "3.8.0"
     fi
 }
 
-function linux() {
+function llvm_download() {
     export LLVM_VERSION_TRIPLE=`llvm_version_triple ${LLVM_VERSION}`
-    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-linux-gnu-ubuntu-14.04
+    export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-$1
 
     wget http://llvm.org/releases/${LLVM_VERSION_TRIPLE}/${LLVM}.tar.xz
     mkdir llvm
@@ -23,11 +23,8 @@ function linux() {
     export LLVM_CONFIG_PATH=`pwd`/llvm/bin/llvm-config
 }
 
-function osx() {
-    brew update >/dev/null
-    brew install llvm3${LLVM_VERSION#3.}
-
-    export LLVM_CONFIG_PATH=`brew --prefix llvm3${LLVM_VERSION#3.}`/lib/llvm-${LLVM_VERSION}/bin/llvm-config
-}
-
-if [ "${TRAVIS_OS_NAME}" == "osx" ]; then osx; else linux; fi
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    llvm_download linux-gnu-ubuntu-14.04
+else
+    llvm_download apple-darwin
+fi
--- ci/script.sh
+++ ci/script.sh
@@ -1,8 +1,8 @@
 set -e
 
-RUST_BACKTRACE=1 cargo test --verbose --features $CLANG_VERSION;
+RUST_BACKTRACE=1 cargo test --verbose --features $CLANG_VERSION -- --nocapture
 
 if [ "${CLANG_VERSION}" \< "clang_3_7" ]; then
     cargo clean
-    RUST_BACKTRACE=1 cargo test --verbose --features "$CLANG_VERSION static"
+    RUST_BACKTRACE=1 cargo test --verbose --features "$CLANG_VERSION static" -- --nocapture
 fi
