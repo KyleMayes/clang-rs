56f3ee682c277f75b43b28782f662bb67ad10317
Tyler Mandry <tmandry@gmail.com>
2020-07-01 13:30:42 -0700

    Fix AppVeyor build (#29)
    Update to https:// URL for LLVM downloads, set to follow redirects for future moves.

--- ci/install.bat
+++ ci/install.bat
@@ -1,8 +1,8 @@
-curl -sSf https://static.rust-lang.org/dist/rust-1.37.0-i686-pc-windows-msvc.exe -o rust.exe
+curl -sSLf https://static.rust-lang.org/dist/rust-1.37.0-i686-pc-windows-msvc.exe -o rust.exe
 rust.exe /VERYSILENT /NORESTART /DIR="C:\Rust"
 set PATH=%PATH%;C:\Rust\bin
 
-curl -sSf http://releases.llvm.org/%LLVM_VERSION%/LLVM-%LLVM_VERSION%-win32.exe -o LLVM.exe
+curl -sSLf https://releases.llvm.org/%LLVM_VERSION%/LLVM-%LLVM_VERSION%-win32.exe -o LLVM.exe
 7z x LLVM.exe -oC:\LLVM
 set PATH=%PATH%;C:\LLVM\bin
 set LIBCLANG_PATH=C:\LLVM\bin
