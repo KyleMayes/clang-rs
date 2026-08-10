46dc1f895b0c987a9a9c6326bc33a24a83e32e39
Mads Marquart <mads@marquart.dk>
2026-08-10 19:01:12 +0200

    Run cargo fmt -pbindgen-test

--- bindgen-test/src/main.rs
+++ bindgen-test/src/main.rs
@@ -5,6 +5,9 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
-    let structure = Structure { foo: 322, bar: 6.44 };
+    let structure = Structure {
+        foo: 322,
+        bar: 6.44,
+    };
     println!("{:?}", structure);
 }
